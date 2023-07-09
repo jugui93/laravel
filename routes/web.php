400ac5fe69f42ef8d88671cd9da575c40d76f626
Juan Morales <juanguillermo93@gmail.com>
2023-07-09 11:48:39 -0500

    views definition

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,32 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+|Route::get Consultar
+|Route::post Guardar 
+|Route::delete Eliminar 
+|Route::put Actualizar 
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('blog', function () {
+    //consulta a base de datos
+    $posts = [
+        ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
+        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel']
+    ];
+
+    return view('blog',['posts'=>$posts]);
+});
+Route::get('blog/{slug}', function ($slug) {
+    //Consulta abase de datos
+    $post = $slug;
+
+    return view('post', ['post' => $post]);
+});
+
