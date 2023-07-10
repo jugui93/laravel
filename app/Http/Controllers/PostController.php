682d57e6397a6e9a82790930f10b5d1130c0785d
Juan Morales <juanguillermo93@gmail.com>
2023-07-10 00:09:41 -0500

    implementation of  destroy  method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        return view('posts.index',[
+            'posts'=>Post::latest()->paginate()
+        ]);
+    }
+    public function create()
+    {
+        return view('posts.create');
+    }
+    public function index()
+    {
+        return view('posts.index',[
+            'posts'=>Post::latest()->paginate()
+        ]);
+    }
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return back();
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,29 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Listado de publicaciones") }}
+                    <table class="mb-4">
+                        @foreach ($posts as $post)
+                        <tr class= "border-b border-gray-200 text-sm">
+                            <td class="px-6 py-4">{{ $post->title }}</td>
+                            <td class="px-6 py-4">
+                                <a href="" class="text-indigo-600">Editar</a>
+                            </td>
+                            <td class="px-6 py-4">
+                                <form action="{{ route('posts.destroy', $post)}}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <input
+                                        type="submit" 
+                                        value="Eliminar" 
+                                        class="bg-gray-800 text-white rounded px-4 py-2"
+                                        onclick="return confirm('Desea Eliminar?')"
+                                        >
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </table>
+                    {{ $posts->links() }}
                 </div>
             </div>
         </div>
