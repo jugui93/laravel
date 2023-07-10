1b791a4804b270f7fd6a6f6878fe7b3123b0af6f
Juan Morales <juanguillermo93@gmail.com>
2023-07-10 13:11:34 -0500

    add slug validation

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -5,6 +5,11 @@
 <input type="text" name="title" class="rounded border-gray-200 w-full mb-4" 
     value="{{ old('title', $post->title) }}"
 >
+<label for="" class="uppercase text-gray-700 text-xs">Slug</label>
+<span class="text-xs text-red-600">@error('slug') {{ $message }} @enderror</span>
+<input type="text" name="slug" class="rounded border-gray-200 w-full mb-4" 
+    value="{{ old('slug', $post->slug) }}"
+>
 <label for="" class="uppercase text-gray-700 text-xs">Contenido</label>
 <span class="text-xs text-red-600">@error('body') {{ $message }} @enderror</span>
 <textarea name="body" rows="5" class="rounded border-gray-200 w-full mb-4">
