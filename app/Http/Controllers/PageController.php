8af5e37a352572f25af4c97404e0b86db1a15477
Juan Morales <juanguillermo93@gmail.com>
2023-07-10 14:11:43 -0500

    styling for views

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,22 +8,23 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     public function home()
-    {
-        return view('home');
-    }
-    public function blog()
-    {
-        //consulta a base de datos
-        // $posts = Post::get();
-        // $post = Post::first();
-        // $post = Post::find(25);
-
-        // dd($post);
-
+    {   
         $posts = Post::latest()->paginate();
-
-        return view('blog',['posts'=>$posts]);
+        return view('home',['posts'=>$posts]);
     }
+    // public function blog()
+    // {
+    //     //consulta a base de datos
+    //     // $posts = Post::get();
+    //     // $post = Post::first();
+    //     // $post = Post::find(25);
+
+    //     // dd($post);
+
+    //     $posts = Post::latest()->paginate();
+
+    //     return view('blog',['posts'=>$posts]);
+    // }
     public function post(Post $post)
     {
         return view('post', ['post' => $post]);
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
 
 Route::controller(PageController::class)->group( function () {
-    Route::get('/',           'home')->name('home');
-    Route::get('blog',        'blog')->name('blog');
+    Route::get('/',                'home')->name('home');
     Route::get('blog/{post:slug}', 'post')->name('post');
 });
 
